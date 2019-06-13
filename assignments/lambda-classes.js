e732e87f1751cfc971709c5d4b54ea6c18174859
bigdre4557@gmail.com <amwilson2019@gmail.com>
2019-06-13 15:09:43 -0400

    lambda,js creating classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = location.age;
+    }
+    speak() {
+        console.log()
+    }
+}
