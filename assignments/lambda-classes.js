da4c3dd893ecc8773318f74c9352c9bbc7a67c6d
bigdre4557@gmail.com <amwilson2019@gmail.com>
2019-06-13 15:40:14 -0400

    One more class, lambda.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,20 +3,34 @@ class Person {
     constructor(props) {
         this.name = props.name;
         this.age = props.age;
-        this.location = location.age;
+        this.location = props.location;
     }
     speak() {
-        console.log()
+        return(`Hello, my name is ${this.name} and I'm from ${this.location}`);
     }
 }
 
-class Instructor {
-  constructor(instructorarrts) {
-      this.specialty = instructorarrts.specialty;
-      this.favLanguage = instructorarrts.favLanguage;
-      this.catchPhrase = instructorarrts.catchPhrase;
+class Instructor extends Person {
+  constructor() {
+    super(instructorarrts);
+        this.specialty = instructorarrts.specialty;
+        this.favLanguage = instructorarrts.favLanguage;
+        this.catchPhrase = instructorarrts.catchPhrase;
   }
   demo(subject) {
-    console.log();
+    return(`Today we are learning about ${subject}`);
   }
+  grade(student, subject) {
+      return(`${student.name} receives a perfect score on ${subject}`);
+  }
+}
+class Student extends Person {
+    constructor(studentArrts) {
+      this.previousBackground = studentArrts.previousBackground;
+      this.className = studentArrts.className;
+      this.favSubjects = studentArrts.favLanguage;
+    }
+    listsSubjects() {
+        return `${this.favSubjects}`;
+    }
 }
